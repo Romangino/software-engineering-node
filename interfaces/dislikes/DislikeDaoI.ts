5dd512823c8cd46e959ee8bfae97dc087679e244
Gino <romangino@gmail.com>
2022-11-17 14:46:00 -0500

    DislikeDao.ts created and implemented

--- interfaces/dislikes/DislikeDaoI.ts
+++ interfaces/dislikes/DislikeDaoI.ts
@@ -4,8 +4,6 @@
 import Dislike from "../../models/dislikes/Dislike";
 
 export default interface DislikeDaoI {
-    findAllUsersThatDislikedTuit(tid: string): Promise<Dislike[]>;
-
     findAllTuitsDislikedByUser(uid: string): Promise<Dislike[]>;
 
     userDislikesTuit(tid: string, uid: string): Promise<Dislike>;
