edeff62c13987452482f722a5caada538f6fb06f
Gino <romangino@gmail.com>
2022-11-20 14:43:10 -0500

    Heroku potential bug fix

--- server.ts
+++ server.ts
@@ -53,7 +53,7 @@ mongoose.connect(connectionString, options);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.CORS_ORIGIN
+    origin: true
 }));
 
 let sess = {
